dff41de2cf00c05d78ecd0d40c3182415a715fa7
Hollin <hollinisme@gmail.com>
2021-10-15 17:45:50 +0800

    make format and finish index scan(without tests to check correctness)

--- PROJECT/bustub/src/execution/index_scan_executor.cpp
+++ PROJECT/bustub/src/execution/index_scan_executor.cpp
@@ -13,10 +13,28 @@
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+  this->plan_ = plan;
+}
 
-void IndexScanExecutor::Init() {}
+void IndexScanExecutor::Init() {
+  Index *target_index = this->exec_ctx_->GetCatalog()->GetIndex(this->plan_->GetIndexOid())->index_.get();
+  this->it_ =
+      reinterpret_cast<BPlusTreeIndex<GenericKey<8>, RID, GenericComparator<8>> *>(target_index)->GetBeginIterator();
+}
 
-bool IndexScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
-
+bool IndexScanExecutor::Next(Tuple *tuple, RID *rid) {
+  while (!this->it_.isEnd()) {
+    auto p = *this->it_;
+    Tuple this_one{p.second};
+    *tuple = this_one;
+    *rid = p.second;
+    if (this->plan_->GetPredicate()->Evaluate(tuple, this->GetOutputSchema()).GetAs<bool>()) {
+      ++this->it_;
+      return true;
+    }
+    ++this->it_;
+  }
+  return false;
+}
 }  // namespace bustub
