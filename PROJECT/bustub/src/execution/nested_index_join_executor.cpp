ac754ed47d7515894117e007f39abc8d7639ddcd
Hollin <hollinisme@gmail.com>
2021-10-28 17:04:38 +0800

    pass join test

--- PROJECT/bustub/src/execution/nested_index_join_executor.cpp
+++ PROJECT/bustub/src/execution/nested_index_join_executor.cpp
@@ -16,10 +16,46 @@ namespace bustub {
 
 NestIndexJoinExecutor::NestIndexJoinExecutor(ExecutorContext *exec_ctx, const NestedIndexJoinPlanNode *plan,
                                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+  this->exec_ctx_ = exec_ctx_;
+  this->plan_ = plan;
+  this->child_executor_ = std::move(child_executor);
+}
 
-void NestIndexJoinExecutor::Init() {}
+void NestIndexJoinExecutor::Init() {
+  this->child_executor_->Init();
+  this->index_info_ = this->exec_ctx_->GetCatalog()->GetIndex(
+      this->plan_->GetIndexName(), this->exec_ctx_->GetCatalog()->GetTable(this->plan_->GetInnerTableOid())->name_);
+  this->table_info_ = this->exec_ctx_->GetCatalog()->GetTable(this->plan_->GetInnerTableOid());
+}
 
-bool NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) {
+  Tuple left_tuple, right_to_search_tuple;
+  RID left_tuple_RID, right_to_search_RID;
+  while (this->child_executor_->Next(&right_to_search_tuple, &right_to_search_RID)) {
+    left_tuple = right_to_search_tuple.KeyFromTuple(*this->plan_->OuterTableSchema(), this->index_info_->key_schema_,
+                                                    this->index_info_->index_->GetKeyAttrs());
+    std::vector<RID> result;
+    this->index_info_->index_->ScanKey(left_tuple, &result, this->exec_ctx_->GetTransaction());
+    if (result.size() > 0) {
+      assert(result.size() == 1);
+      Tuple t;
+      this->table_info_->table_->GetTuple(result[0], &t, this->exec_ctx_->GetTransaction());
+      if (this->plan_->Predicate()
+              ->EvaluateJoin(&t, this->plan_->InnerTableSchema(), &right_to_search_tuple,
+                             this->plan_->OuterTableSchema())
+              .GetAs<bool>()) {
+        std::vector<Value> values;
+        for (auto &col : GetOutputSchema()->GetColumns()) {
+          values.push_back(col.GetExpr()->EvaluateJoin(&t, this->plan_->InnerTableSchema(), &right_to_search_tuple,
+                                                       this->plan_->OuterTableSchema()));
+        }
+        *tuple = Tuple(values, GetOutputSchema());
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 }  // namespace bustub
--- PROJECT/bustub/src/execution/nested_loop_join_executor.cpp
+++ PROJECT/bustub/src/execution/nested_loop_join_executor.cpp
@@ -18,31 +18,35 @@ NestedLoopJoinExecutor::NestedLoopJoinExecutor(ExecutorContext *exec_ctx, const 
                                                std::unique_ptr<AbstractExecutor> &&left_executor,
                                                std::unique_ptr<AbstractExecutor> &&right_executor)
     : AbstractExecutor(exec_ctx) {
-        this->exec_ctx_ = exec_ctx;
-        this->plan_ = plan;
-        this->left_executor_ = std::move(left_executor);
-        this->right_executor_ = std::move(right_executor);
-    }
-
-void NestedLoopJoinExecutor::Init() {
-    this->left_executor_.get()->Init();
+  this->exec_ctx_ = exec_ctx;
+  this->plan_ = plan;
+  this->left_executor_ = std::move(left_executor);
+  this->right_executor_ = std::move(right_executor);
 }
 
+void NestedLoopJoinExecutor::Init() { this->left_executor_.get()->Init(); }
+
 bool NestedLoopJoinExecutor::Next(Tuple *tuple, RID *rid) {
-    Tuple left_tuple, right_tuple;
-    RID left_tuple_rid, right_tuple_rid;
-    while(this->left_executor_->Next(&left_tuple, &left_tuple_rid)){
-        this->right_executor_.get()->Init();
-        while(this->right_executor_->Next(&right_tuple, &right_tuple_rid)){
-          if (this->plan_->Predicate()->EvaluateJoin(&left_tuple, this->plan_->GetLeftPlan()->OutputSchema(),
-                                                     &right_tuple, this->plan_->GetRightPlan()->OutputSchema()).GetAs<bool>()){
-            *tuple = left_tuple;
-            *rid = left_tuple_rid;
-            return true;
-          }
+  Tuple left_tuple, right_tuple;
+  RID left_tuple_rid, right_tuple_rid;
+  while (this->left_executor_->Next(&left_tuple, &left_tuple_rid)) {
+    this->right_executor_.get()->Init();
+    while (this->right_executor_->Next(&right_tuple, &right_tuple_rid)) {
+      if (this->plan_->Predicate()
+              ->EvaluateJoin(&left_tuple, this->plan_->GetLeftPlan()->OutputSchema(), &right_tuple,
+                             this->plan_->GetRightPlan()->OutputSchema())
+              .GetAs<bool>()) {
+        std::vector<Value> values;
+        for (auto &col : GetOutputSchema()->GetColumns()) {
+          values.push_back(col.GetExpr()->EvaluateJoin(&left_tuple, this->left_executor_->GetOutputSchema(),
+                                                       &right_tuple, this->right_executor_->GetOutputSchema()));
         }
+        *tuple = Tuple(values, GetOutputSchema());
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 }  // namespace bustub
