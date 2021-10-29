822ab31bca97403375fac63eb8a4dc5ebe83bee1
Hollin <hollinisme@gmail.com>
2021-10-29 15:57:44 +0800

    finish limit and make format

--- PROJECT/bustub/src/execution/limit_executor.cpp
+++ PROJECT/bustub/src/execution/limit_executor.cpp
@@ -16,28 +16,24 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx),
-    plan_(plan), 
-    child_executor_(std::move(child_executor)) {
-        this->exec_ctx_ = exec_ctx_;
-        this->counter_ = 0;
-        this->offset_ = 0;
-    }
-
-void LimitExecutor::Init() {
-    this->child_executor_->Init();
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {
+  this->exec_ctx_ = exec_ctx_;
+  this->counter_ = 0;
+  this->offset_ = 0;
 }
 
+void LimitExecutor::Init() { this->child_executor_->Init(); }
+
 bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
-    while(this->counter_ < this->plan_->GetLimit() && this->child_executor_->Next(tuple, rid)){
-        if(this->offset_ < this->plan_->GetOffset()){
-            this->offset_++;
-            continue;
-        }
-        this->counter_++;
-        return true;
+  while (this->counter_ < this->plan_->GetLimit() && this->child_executor_->Next(tuple, rid)) {
+    if (this->offset_ < this->plan_->GetOffset()) {
+      this->offset_++;
+      continue;
     }
-    return false;
+    this->counter_++;
+    return true;
+  }
+  return false;
 }
 
 }  // namespace bustub
