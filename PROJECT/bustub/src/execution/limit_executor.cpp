3f9961ac318739f69e11f7f55f57ba97dd5fb630
Hollin <hollinisme@gmail.com>
2021-10-29 15:57:00 +0800

    finish limit

--- PROJECT/bustub/src/execution/limit_executor.cpp
+++ PROJECT/bustub/src/execution/limit_executor.cpp
@@ -16,10 +16,28 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+    plan_(plan), 
+    child_executor_(std::move(child_executor)) {
+        this->exec_ctx_ = exec_ctx_;
+        this->counter_ = 0;
+        this->offset_ = 0;
+    }
 
-void LimitExecutor::Init() {}
+void LimitExecutor::Init() {
+    this->child_executor_->Init();
+}
 
-bool LimitExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
+    while(this->counter_ < this->plan_->GetLimit() && this->child_executor_->Next(tuple, rid)){
+        if(this->offset_ < this->plan_->GetOffset()){
+            this->offset_++;
+            continue;
+        }
+        this->counter_++;
+        return true;
+    }
+    return false;
+}
 
 }  // namespace bustub
